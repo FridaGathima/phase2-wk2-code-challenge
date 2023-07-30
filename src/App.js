e187a4d6f4ad6507d8143bdb4e15e3453d7d8ac3
+ <+>
2023-07-30 21:57:32 +0300

    App.js edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import BotCollection from "./components/BotCollection";
-import YourBotArmy from "./components/YourBotArmy"
 import Bots from "./components/Bots";
 
 // import './App.css';
